e6ada9a2c8f7d91ac5846e37a914f36dbf1c2cf8
haru-voster <harounbravo2@gmail.com>
2024-11-21 15:12:43 +0200

    ADMIN setup for reactweb and mongodb

---
+++ src/assets/assets.js
@@ -0,0 +1,16 @@
+import logo from './logo.png'
+import add_icon from './add_item.png'
+import order_icon from './order_icon.jpg'
+import profile_icon from './profile_icon.jpg'
+import upload_icon from './upload_icon.png'
+import parcel_icon from './parcel_icon.jpg'
+
+export const assets = {
+
+    logo,
+    add_icon,
+    order_icon,
+    profile_icon,
+    upload_icon,
+    parcel_icon
+}
